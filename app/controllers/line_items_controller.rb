c0b4c9e8985fe2c49b2bf2a4440eed9b35e1dfd9
kajsa kristersson <k.kristersson@gmail.com>
2017-11-09 23:51:48 +0100

    conrtoller action added

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
-
+  include CurrentCart
+  before_action :set_cart, only: [:create]
   # GET /line_items
   # GET /line_items.json
   def index
